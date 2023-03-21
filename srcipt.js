e91e13fdba44d139828239a34df60f914f3ffcea
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2023-03-21 17:06:25 +0000

    fix

--- srcipt.js
+++ srcipt.js
@@ -3,5 +3,5 @@ fetch("https://raw.githubusercontent.com/TheCoderEll/ttrshax/main/html.html")
       return response.text();
     })
     .then((data) => {
-        document.querySelector("#version-4\\.22\\.5271028").append(data)
+        document.querySelector("#version-4\\.22\\.5271028").innerHTML += (data.slice(1,-1))
     });
