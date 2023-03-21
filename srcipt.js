f817582f6729b16ac683f5b5d515db42243a55c4
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2023-03-21 17:02:11 +0000

    .

--- srcipt.js
+++ srcipt.js
@@ -1,1 +1,7 @@
-console.log("YAY")
+fetch("https://raw.githubusercontent.com/TheCoderEll/ttrshax/main/html.html")
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+        document.querySelector("#version-4\\.22\\.5271028").append(data)
+    });
