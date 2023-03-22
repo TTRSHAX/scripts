120710f30e3817e19031c7d154dbbf89ace36aa8
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2023-03-22 16:18:41 +0000

    remove the html injetc

--- srcipt.js
+++ srcipt.js
@@ -1,7 +1,1 @@
-fetch("https://raw.githubusercontent.com/TheCoderEll/ttrshax/main/html.html")
-    .then((response) => {
-      return response.text();
-    })
-    .then((data) => {
-        document.querySelector("#version-4\\.22\\.5271028").innerHTML += (data.slice(1,-1))
-    });
+console.log("TEST");
