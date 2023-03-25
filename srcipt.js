98ab6f83c6db97012e1aebf010286abd7276b69a
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2023-03-25 08:24:36 +0000

    push

--- srcipt.js
+++ srcipt.js
@@ -1,54 +1,75 @@
 var run = false;
-function ans(){
-	try {
-		//get question
-	q = document.querySelector("#version-4\\.22\\.5271028 > ttr-root > ttr-root-app > div > mat-sidenav-container > mat-sidenav-content > div > section > ttr-garage > ttr-game-holder > div > div.game.game-holder-inner.ng-star-inserted > div.pedal-holder > ttr-game-footpedal > section.width-100 > section > section > ttr-game-question > span").innerHTML;
-	//split for digestion
-	text = q.split(" ");
-	// understand
-		if (q.includes("÷")) {
-			no1 = text[1];
-			op = text[2].split("<!---->")[1];
-			no2 = text[3];
-		} else {
-			no1 = text[1];
-			op = text[3].split("<!---->")[0];
-			no2 = text[4];
-		}
-		//init ans var
-		answer = "";
-		//answer q
-		if (op == "×") {
-			answer = parseInt(no1) * parseInt(no2);
-		}
-		if (op == "÷") {
-			answer = parseInt(no1) / parseInt(no2);
-		}
-		//format
-		answer = String(answer);
-		nol = answer.split('');
-		//enter
-		for (let i = 0; i < nol.length; i++){
-			simulateKeydown (48 + parseInt(nol[i]), false, false, false);
-		}
-		simulateKeydown (13, false, false, false);} catch {}
+
+function ans() {
+    try {
+        //get question
+        q = document.querySelector("#version-4\\.22\\.5271028 > ttr-root > ttr-root-app > div > mat-sidenav-container > mat-sidenav-content > div > section > ttr-garage > ttr-game-holder > div > div.game.game-holder-inner.ng-star-inserted > div.pedal-holder > ttr-game-footpedal > section.width-100 > section > section > ttr-game-question > span").innerHTML;
+        //split for digestion
+        text = q.split(" ");
+        // understand
+        if (q.includes("÷")) {
+            no1 = text[1];
+            op = text[2].split("<!---->")[1];
+            no2 = text[3];
+        } else {
+            no1 = text[1];
+            op = text[3].split("<!---->")[0];
+            no2 = text[4];
+        }
+        //init ans var
+        answer = "";
+        //answer q
+        if (op == "×") {
+            answer = parseInt(no1) * parseInt(no2);
+        }
+        if (op == "÷") {
+            answer = parseInt(no1) / parseInt(no2);
+        }
+        //format
+        answer = String(answer);
+        nol = answer.split('');
+        //enter
+        for (let i = 0; i < nol.length; i++) {
+            simulateKeydown(48 + parseInt(nol[i]), false, false, false);
+        }
+        simulateKeydown(13, false, false, false);
+    } catch {}
 }
-function simulateKeydown (keycode,isCtrl,isAlt,isShift){
-	var e = new KeyboardEvent( "keydown", { bubbles:true, cancelable:true, char:String.fromCharCode(keycode), key:String.fromCharCode(keycode), shiftKey:isShift, ctrlKey:isCtrl, altKey:isAlt } );
-        Object.defineProperty(e, 'keyCode', {get : function() { return this.keyCodeVal; } });     
-        e.keyCodeVal = keycode;
-        document.dispatchEvent(e);
+
+function simulateKeydown(keycode, isCtrl, isAlt, isShift) {
+    var e = new KeyboardEvent("keydown", {
+        bubbles: true,
+        cancelable: true,
+        char: String.fromCharCode(keycode),
+        key: String.fromCharCode(keycode),
+        shiftKey: isShift,
+        ctrlKey: isCtrl,
+        altKey: isAlt
+    });
+    Object.defineProperty(e, 'keyCode', {
+        get: function() {
+            return this.keyCodeVal;
+        }
+    });
+    e.keyCodeVal = keycode;
+    document.dispatchEvent(e);
 }
+
 function iamdaone() {
-  //answer
-  console.log("DATAA")
-  if (run) ans();
+    //answer
+    if (run) ans();
 }
 
 function genRand(min, max) {
     return (Math.random() * (max - min) + min);
 }
-//run
-setTimeout(() => { iamdaone(); }, genRand(500,1000))
-setTimeout(() => {
-document.getElementById("0").addEventListener("click", () => {run = !run});}, 1000)
+
+setInterval(() => {
+    iamdaone();
+}, 1000);
+
+window.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("0").addEventListener("click", () => {
+        run = !run
+    }, false);
+});
