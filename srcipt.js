30771aca6f048a9f744371f661446510bf120e8d
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2023-07-04 16:21:54 +0100

    Update srcipt.js
    Well that one didnt work as intended

--- srcipt.js
+++ srcipt.js
@@ -1,18 +1,6 @@
 var run = false;
 var repete = false;
 
-function getRequests() {
-    var s1 = location.search.substring(1, location.search.length).split('&'),
-        r = {}, s2, i;
-    for (i = 0; i < s1.length; i += 1) {
-        s2 = s1[i].split('=');
-        r[decodeURIComponent(s2[0]).toLowerCase()] = decodeURIComponent(s2[1]);
-    }
-    return r;
-};
-
-var QueryString = getRequests();
-
 function ans() {
   try {
     //get question
@@ -79,9 +67,17 @@ function genRand(min, max) {
   return Math.random() * (max - min) + min;
 }
 
+const self = document.currentScript;
+  const v = new URLSearchParams(
+    new URL(self.getAttribute('_src')).search,
+  ).get('s');
+
+s = parseInt(w)
+console.log(s)
+
 setInterval(() => {
   iamdaone();
-}, parseInt(QueryString["s"]));
+}, s);
 
 setTimeout(() => {
   document.getElementById("0").addEventListener(
